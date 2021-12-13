eb70115fdc1af4fe6a5b1b9ed9a4c34a58dff869
Anna <an9062@gmail.com>
2021-12-13 20:02:59 +1100

    added blog and comment routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -4,5 +4,35 @@ const withAuth = require("../../utils/auth");
 const { Op } = require("sequelize");
 
 // Post
+router.post("/", withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.create({
+            ...req.body,
+            userId = req.session.userId,
+        });
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // Delete
+router.delete("/:id", withAuth, async (req,res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id = req.params.id,
+                userId = req.session.userId,
+            },
+        });
+        if (!commentData) {
+            res.status(404).json({message: "No blog post found!"});
+            return;
+        }
+        res.status (200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
